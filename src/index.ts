38e04e3f8c80983f1dbd772a7b7fbc7a6326eaf2
TK <leandrotk100@gmail.com>
2020-04-03 09:49:41 -0300

    Add templates and configs into the data folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-fs.readFile(path.join(__dirname, '../template.html'), 'utf8', (error, data) => {
+fs.readFile(path.join(__dirname, '../data/template.html'), 'utf8', (error, data) => {
   // console.log(data);
 });
