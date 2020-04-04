b6126c46feee120ffab8bc42a99deec2dc98cb12
TK <leandrotk100@gmail.com>
2020-04-03 22:36:02 -0300

    get template and article config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import { promises as fs } from "fs";
+import { resolve } from "path";
 
-fs.readFile(path.join(__dirname, '../data/template.html'), 'utf8', (error, data) => {
-  // console.log(data);
-});
+const replaceAll = (content: string, pattern: string, replacement: string): string =>
+  content.replace(new RegExp(pattern, "g"), replacement);
+
+const start = async () => {
+  const contentTemplatePath = resolve(__dirname, '../data/template.html');
+  const content = await fs.readFile(contentTemplatePath, "utf8");
+
+  const articleConfigPath = resolve(__dirname, '../data/article.config.json');
+  const articleConfigContent = await fs.readFile(articleConfigPath, "utf8");
+  const articleConfig = JSON.parse(articleConfigContent);
+
+  const {
+    title,
+    description,
+    date,
+    tags,
+    imageCover,
+    photographerUrl,
+    photographerName,
+    articleFile,
+    keywords
+  } = articleConfig;
+};
+
+start();
